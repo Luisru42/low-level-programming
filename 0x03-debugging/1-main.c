d8002de58601bf052d745bd9ce93340f160081c5
Luis Gonzalez <gonzalez.engineer42@gmail.com>
2025-08-21 18:39:44 -0400

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(void)
+{
+    int i;
+
+    printf("Infinite loop incoming :(\n");
+    i = 0;
+
+    while (i < 10)
+    {
+        i++;
+    }
+    printf("Infinite loop avoided! \\o/\n");
+
+    return (0);
+}
