949ddc33ecd2c3c23d257eb19ea87bc826daa3f9
Luis Gonzalez <gonzalez.engineer42@gmail.com>
2025-08-23 18:32:49 -0400

    argument counts and vector

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 3)
+    {
+        int num1 = atoi(argv[1]);
+        int num2 = atoi(argv[2]);
+        printf("%d\n", num1 * num2);
+    }
+    else
+    {
+        printf("Error\n");
+    }
+    return (0);
+}
